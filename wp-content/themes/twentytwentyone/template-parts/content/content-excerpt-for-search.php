10d44663f2547423d1e9cfdaa6e701d92e063f14
ngocty <nb447105@gmail.com>
2023-10-27 23:41:09 +0700

    complete content 5

---
+++ wp-content/themes/twentytwentyone/template-parts/content/content-excerpt-for-search.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Template part for displaying post archives and search results
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> style="display: flex; justify-content: center;margin: 20px 0;">
+	<div class="group-main-post search-post">
+		<div class="image-post">
+			<?php twenty_twenty_one_post_thumbnail(); ?>
+		</div>
+		<div class="date">
+			<?php
+			$post = get_post();
+			$post_date = get_the_date('d', $post->ID);
+			$post_month = get_the_date('m', $post->ID)
+			?>
+			<div class="day"><?php echo $post_date; ?></div>
+			<div class="month">Tháng <?php echo $post_month; ?></div>
+		</div>
+		<div class="wrapper">
+			<div class="line"></div>
+		</div>
+		<div class="group">
+			<?php get_template_part('template-parts/header/excerpt-header', get_post_format()); ?>
+
+			<div class="entry-content">
+				<?php get_template_part('template-parts/excerpt/excerpt', get_post_format()); ?>
+			</div><!-- .entry-content -->
+		</div>
+	</div>
+
+</article><!-- #post-${ID} -->
